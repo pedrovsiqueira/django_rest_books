a6068d17014d557783d3d708ddddecc3301f4cfa
Pedro Siqueira <pedro.v.siqueira@gmail.com>
2024-07-14 15:20:20 -0300

    feat: create django_rest_framework api for books

---
+++ books/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    published_date = models.DateField(auto_now_add=True)
+    isbn_number = models.CharField(max_length=13)
+
+    def __str__(self):
+        return self.title
---
+++ books/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import Book
+from .serializers import BookSerializer
+from .filters import BookFilter
+
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset =  Book.objects.all()
+    serializer_class = BookSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = BookFilter
