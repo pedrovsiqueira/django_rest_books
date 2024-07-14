99d503355dc9e72de9038c6bf9404992367b47b3
Pedro Siqueira <pedro.v.siqueira@gmail.com>
2024-07-14 15:24:19 -0300

    fix: add unique to book title

--- books/models.py
+++ books/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Book(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=100, unique=True)
     author = models.CharField(max_length=100)
     published_date = models.DateField(auto_now_add=True)
     isbn_number = models.CharField(max_length=13)
